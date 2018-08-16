6360b4717f2c835b3cbeb35769385df535155a9f
tiboitel <jules.boitelle@gmail.com>
2018-08-16 23:47:02 +0200

    Update searchengine.

--- src/Controllers/SearchController.php
+++ src/Controllers/SearchController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 use Slim\Http\Request;
 use Slim\Http\Response;
 use Lazer\Classes\Database as Lazer;
+use pdeans\Http\Client;
 
 class SearchController extends AbstractController
 {
@@ -18,12 +19,33 @@ class SearchController extends AbstractController
 	public function show(Request $request, Response $response)
 	{
 		$response = $this->renderer->render($response, 'index.phtml');
-		return ($response);
+		return $response;
 	}
 
-	public function update(Request $request, Response $response)
-	{
-		return ($response);
+	public function search(Request $request, Response $response, $args)
+	{	
+		$client = new Client([
+			CURLOPT_SSL_VERIFYPEER => 0,
+			CURLOPT_SSL_VERIFYHOST => 0
+		]);
+		$args = $request->getParsedBody();
+		$search_data = [];
+		$search_data['request'] = filter_var($args['searchRequest']);
+		if (isset($args['searchInExactTerms']) && !empty($args['searchInExactTerms']))
+			$search_data['request'] = '\'' . $search_data['request'] . '\'';
+		if (isset($args['searchInTitle']) && !empty($args['searchInTitle']))
+			$search_data['searchInTitle'] = true;
+		$search_data['searchNotInclude'] = filter_var($args['searchNotInclude']);
+		$base_url = 'https://www.leboncoin.fr/recherche/?text=';
+		$base_url .= urlencode($search_data['request']);
+		$base_url .= "&regions=12";
+		if ($search_data['searchInTitle'] == true)
+			$base_url .= "&search_in=subject";
+		$base_url .= "&sort=price&order=asc";
+		$http_response = $client->get($base_url);
+		$header = file_get_contents("../templates/index.phtml");
+		$header .= $http_response->getBody();
+		$response->getBody()->write($header);
 	}
 
 	public function validateSearchRequest($value, $user_id)
--- src/routes.php
+++ src/routes.php
@@ -3,11 +3,10 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
-$app->group('/search', 
+$app->group('', 
 	function() 
 	{
-		$this->get('/', \App\Controllers\SearchController::class . ':show');
+		$this->get('/', \App\Controllers\SearchController::class . ':show')->setName('search.show');
+		$this->post('/search', \App\Controllers\SearchController::class . ':search')->setName('search.search');
 	}
 );
-
-$app->get('/', 'SearchController:show');
